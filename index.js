a23efb4646bf78d1a25609e4e0bd2a4a2aa5a089
phamkhacan1903 <phamkhacan1903@gmail.com>
2019-02-11 11:41:06 +0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function insertNum(num) {
-
+	var string = document.getElementById('result').value;
+	if(string.startsWith('0') && num != '.') 
+		document.getElementById('result').value = string.replace('0','');
 	document.getElementById('result').value = 
 	document.getElementById('result').value + num;
 } 
@@ -7,14 +9,19 @@ function insertNum(num) {
 function calculate() {
 	var result = document.getElementById('result').value;
 	if(result)
-	document.getElementById('result').value = eval(result);
+		document.getElementById('result').value = Math.round(eval(result) * 10000000)/10000000;
+	
 }
 
 function clean() {
-	document.getElementById('result').value = "";
+	document.getElementById('result').value = "0";
 }
 
 function back() {
 	var exp = document.getElementById('result').value;
+	
 	document.getElementById('result').value = exp.substring(0,exp.length-1);
+	// xoa het thi tra ve '0'
+	if(document.getElementById('result').value == "") 
+		document.getElementById('result').value = "0";
 }
